63f6f98c8e582f92fae70cf3f10a37b7aadd1999
Gianni <50083193+CapucheGianni@users.noreply.github.com>
2020-11-21 18:38:08 +0100

    Update affiliate.php

--- lang/en/app/affiliate.php
+++ lang/en/app/affiliate.php
@@ -1,23 +1,23 @@
 <?php
 
-	$AFFILIATE_CARD_1_1 = "Clics";
-	$AFFILIATE_CARD_1_2 = "Affilié(s)";
-	$AFFILIATE_CARD_1_3 = "Niveau";
+	$AFFILIATE_CARD_1_1 = "Clicks";
+	$AFFILIATE_CARD_1_2 = "Affiliate(s)";
+	$AFFILIATE_CARD_1_3 = "Level";
 
 	$AFFILIATE_CARD_2_TITLE = "INFORMATIONS";
 	$AFFILIATE_CARD_2_LEVEL = "LEVEL";
-	$AFFILIATE_CARD_2_IN = "affilié(s) sur"; // xx affilié(s) sur xx !
-	$AFFILIATE_CARD_2_END = "Nombre d'affilié :";
+	$AFFILIATE_CARD_2_IN = "affiliated on"; // xx affiliate(s) on xx !
+	$AFFILIATE_CARD_2_END = "Number of affiliates :";
 
-	$AFFILIATE_CARD_2_LINK = "Votre lien d'affilié :";
+	$AFFILIATE_CARD_2_LINK = "Your affiliate link :";
 
-	$AFFILIATE_CARD_3_TITLE = "LOGS DES AFFILIÉS";
+	$AFFILIATE_CARD_3_TITLE = "Affiliates logs";
 	$AFFILIATE_CARD_3_1 = "Pseudo";
 	$AFFILIATE_CARD_3_2 = "Action";
 	$AFFILIATE_CARD_3_3 = "Gains";
 	$AFFILIATE_CARD_3_3_COINS = "coins";
 
-	$AFFILIATE_CARD_4_TITLE = "VOS AFFILIÉS";
+	$AFFILIATE_CARD_4_TITLE = "Your affiliates";
 	$AFFILIATE_CARD_4_1 = "Pseudo";
 
 ?>

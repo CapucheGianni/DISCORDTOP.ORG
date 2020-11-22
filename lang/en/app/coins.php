e5bc6b0a1d104e74eed2ab4092958107645db6f6
Gianni <50083193+CapucheGianni@users.noreply.github.com>
2020-11-22 17:56:13 +0100

    Update coins.php

--- lang/en/app/coins.php
+++ lang/en/app/coins.php
@@ -1,22 +1,22 @@
 <?php
 
-	$COINS_DEDIPASS_SUCCESS_PART_1 = "Nous vous remercions pour votre confiance et votre fidélité, ";
-	$COINS_DEDIPASS_SUCCESS_PART_2 = " coins viennent de vous être crédités !";
+	$COINS_DEDIPASS_SUCCESS_PART_1 = "We thank you for your confidence and your fidelity, ";
+	$COINS_DEDIPASS_SUCCESS_PART_2 = " coins just have been credited to you !";
 
-	$COINS_DEDIPASS_ERROR_PART_1 = "Le code ";
-	$COINS_DEDIPASS_ERROR_PART_2 = " est invalide !";
+	$COINS_DEDIPASS_ERROR_PART_1 = "The code ";
+	$COINS_DEDIPASS_ERROR_PART_2 = " is wrong !";
 
-	$COINS_DEDIPASS_TITLE = "Créditer mon compte avec Dédipass";
+	$COINS_DEDIPASS_TITLE = "Credit my account with Dedipass";
 	$COINS_DEDIPASS_BALANCE = "My account :";
-	$COINS_DEDIPASS_FOR = "À quoi servent les coins ?";
-	$COINS_DEDIPASS_TEXT = "Pour acheter des mises en avant ainsi que des abonnements... il te faut des coins, les coins sont la monnaie virtuelle de DiscordTop, sans eux tu ne pourras pas obtenir des produits de notre boutique.";
-	$COINS_DEDIPASS_NAME = "Dédipass";
+	$COINS_DEDIPASS_FOR = "What is the utility of the coins ?";
+	$COINS_DEDIPASS_TEXT = "To buy some promotitions and subscriptions... you must have some coins, the coins are the virtual money of DiscordTop, without them you will can't obtain some products of our shop.";
+	$COINS_DEDIPASS_NAME = "Dedipass";
 
 
-	$COINS_GIFT_TITLE = "Créditer mon compte avec un code cadeau";
+	$COINS_GIFT_TITLE = "Credit my account with a gift code";
 	$COINS_GIFT_BALANCE = "My account :";
-	$COINS_GIFT_FOR = "Comment obtenir un code cadeau ?";
-	$COINS_GIFT_TEXT = "Notre Discord est une mine à cadeau, dans celui-ci vous pourrez participer à des AirDrop's, des giveaways et d'autres animations qui vous permettront d'obtenir gratuitement des codes cadeaux !";
+	$COINS_GIFT_FOR = "How to obtain a gift code ?";
+	$COINS_GIFT_TEXT = "Our Discord is a gift mine, in it you will can participate to AirDrop's, giveaways and other animations wich will allow you to obtain free some gift code !";
 	$COINS_GIFT_NAME = "Use a Gift Code";
 	$COINS_GIFT_ENTER = "Enter a gift code !";
 	$COINS_GIFT_ENTER_BUTTON = "Use";
@@ -24,8 +24,8 @@
 
 	$COINS_TRANSFERT_TITLE = "Send money to other user";
 	$COINS_TRANSFERT_BALANCE = "My account :";
-	$COINS_TRANSFERT_FOR = "Les règles :";
-	$COINS_TRANSFERT_TEXT = "Vous pouvez uniquement transférer de l'argent à partir de 100 coins.<br>Attention, il est strictement interdit de demander à vos membres de payer à votre place des coins en échange de lot ou de cadeau ! C'est un don, pas un paiement !!!<br>Votre compte peut-être suspendu à tous moment en cas de non respect de cette règle majeure !";
+	$COINS_TRANSFERT_FOR = "Rules :";
+	$COINS_TRANSFERT_TEXT = "You only can transfer money from 100 coins.<br>Be care, it is strictly forbidden to ask to your members to pay for you some coins in exchange of a prize or gifts ! It's a donation, not a payment !!!<br>Your account can be suspended at every moment in case of disrespect of this important rule !";
 	$COINS_TRANSFERT_NAME = "Wow... You're going to send money to your friend !";
 	$COINS_TRANSFERT_WHO = "Enter a discord user identifier !";
 	$COINS_TRANSFERT_HOW = "How many coins ?!";

f8aefd5d43411dd154a33e67bf01cc04b9b72576
Gianni <50083193+CapucheGianni@users.noreply.github.com>
2020-11-22 18:06:28 +0100

    Update menu.php

--- lang/en/app/menu.php
+++ lang/en/app/menu.php
@@ -1,34 +1,34 @@
 <?php
 
-    $MENU_WELCOME = "Bienvenue !";
+    $MENU_WELCOME = "Welcome !";
 
-    $MENU_PROFILE = "Mon profil";
-    $MENU_SETTINGS = "Paramètres";
+    $MENU_PROFILE = "My profile";
+    $MENU_SETTINGS = "Settings";
     $MENU_SUPPORT = "Support";
-    $MENU_LOGOUT = "Déconnexion";
+    $MENU_LOGOUT = "Logging out";
 
-    $MENU_HOME = "Accueil";
-    $MENU_SERVERS = "Mes seveurs";
-    $MENU_STATS = "Mes stats";
+    $MENU_HOME = "Reception";
+    $MENU_SERVERS = "My severs";
+    $MENU_STATS = "My stats";
     $MENU_HELP = "Support";
     $MENU_AFFILIATE = "Affiliation";
 
-    $MENU_SHOP_TITLE = "Boutique";
-    $MENU_SHOP_ADDCOIN = "Créditer des coins";
-    $MENU_SHOP_ADDCOIN_1 = "Dédipass";
-    $MENU_SHOP_ADDCOIN_2 = "Code cadeau";
-    $MENU_SHOP_ADDCOIN_3 = "Transférer des coins";
-    $MENU_SHOP_SHOP = "Boutique";
+    $MENU_SHOP_TITLE = "Shop";
+    $MENU_SHOP_ADDCOIN = "Credit coins";
+    $MENU_SHOP_ADDCOIN_1 = "Dedipass";
+    $MENU_SHOP_ADDCOIN_2 = "Gift code";
+    $MENU_SHOP_ADDCOIN_3 = "Transfer coins";
+    $MENU_SHOP_SHOP = "Shop";
 
     $MENU_DEV_TITLE = "Developer";
     $MENU_DEV_APP = "Application";
 
     $MENU_PARTNERS_TITLE = "Partners";
-    $MENU_PARTNERS_SETTINGS = "Paramètres";
+    $MENU_PARTNERS_SETTINGS = "Settings";
 
-    $MENU_BACKTOSITE = "Retourner sur le site";
+    $MENU_BACKTOSITE = "Go back on the website";
 
-    $MENU_NOTIFICATION_TITLE_PART1 = "Vous avez ";
+    $MENU_NOTIFICATION_TITLE_PART1 = "You have ";
     $MENU_NOTIFICATION_TITLE_PART2_1 = "notification.";
     $MENU_NOTIFICATION_TITLE_PART2_2 = "notifications.";
 
